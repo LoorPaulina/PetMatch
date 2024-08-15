5a3179e2e39f583c265fb756f30a119878ec3a32
Paulina Loor Tubay <108084639+LoorPaulina@users.noreply.github.com>
2024-08-15 14:41:47 -0500

    endpoint registro y getMascotas

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify, request
+from flask import Flask, json,jsonify, request
 from flask_cors import CORS
 import mysql.connector
 #creo el proyecto flask 
@@ -7,16 +7,17 @@ app= Flask(__name__)
 CORS(app)
 #creo la conexion
 
-db=mysql.connector.connect(
+def obtener_conexion():
+    return mysql.connector.connect(
     host="localhost",
     user="root",
     password="root",
     database="petmatch"
-)
+    )
 
 @app.route('/login',methods=['POST'])
 def login():
-
+    db = obtener_conexion()
     data=request.json
     email=data.get('email')
     #password pasada al endpoint 
@@ -35,6 +36,57 @@ def login():
                 "msg":"failed",
                 "user":None,
           }),401
+
+@app.route('/createAccount',methods=['POST'])
+def createAccount():
+    db = obtener_conexion()
+    try:
+      data=request.json
+      email=data.get('email')
+      username=data.get('username')
+      nombre=data.get('nombre')
+      apellido=data.get('apellido')
+      ocupacion=data.get('ocupacion')
+      #password pasada al endpoint 
+      password=data.get('password')
+
+      cursor=db.cursor(dictionary=True)
+      cursor.execute("INSERT INTO Usuario (email, contrasena, nombre, apellido, num_adopciones, cantidad_donaciones, ocupacion, biografia, carta_motivacional, rol_de_pago) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                                    (email, password, nombre, apellido, 0, 0, ocupacion, '', '', ''))
+      db.commit()
+      return jsonify({"msg": "Account created successfully!"}), 201
+    except Exception as e:
+      print(e)
+      db.rollback()
+      return jsonify({"msg": "Failed to create account", "error": str(e)}), 500
+    finally:
+         db.close()
+
+@app.route('/getMascotas',methods=['GET'])
+def getMascotas():
+    conexion = obtener_conexion()
+    mascotas = []
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT * FROM Animal")
+        mascotas = cursor.fetchall()
+    conexion.close()
+    
+    mascotas_json = []
+    for mascota in mascotas:
+        print(mascota)
+        mascota_dict = {
+            'nombre': mascota[0],
+            'edad': mascota[1],
+            'peso': mascota[2],
+            'sexo': mascota[3],
+            'estado_adopcion': mascota[4],
+            'altura': mascota[5]
+        }
+        mascotas_json.append(mascota_dict)
+
+
+    return jsonify({"data": mascotas_json}), 201
+
 
 
 if __name__ == '__main__':
